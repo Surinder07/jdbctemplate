1a13a7a15e445f14e1d929527b43085f8de865ef
Surinder007 <surinder0322@gmail.com>
2021-10-02 21:08:31 -0400

    CRUD template

--- src/main/java/io/pragra/learning/springjdbclearning/SpringjdbcLearningApplication.java
+++ src/main/java/io/pragra/learning/springjdbclearning/SpringjdbcLearningApplication.java
@@ -1,6 +1,7 @@
 package io.pragra.learning.springjdbclearning;
 
 import io.pragra.learning.springjdbclearning.dao.UserDAO;
+import io.pragra.learning.springjdbclearning.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +16,18 @@ public class SpringjdbcLearningApplication {
 
         UserDAO dao = context.getBean(UserDAO.class);
         dao.readAndprintUsers();
+
+      //  System.out.println(dao.addUser(new User(101, "shubham", "Singh")));
+
+        User user = dao.findUserById(102);
+        user.setLastName("S");
+        dao.updateUser(user);
+
+       // dao.creatStudentTable();
+
+        dao.findAllByLastName("Singh").forEach(System.out::println);
+
+
     }
 
 }
--- src/main/java/io/pragra/learning/springjdbclearning/dao/UserDAO.java
+++ src/main/java/io/pragra/learning/springjdbclearning/dao/UserDAO.java
@@ -16,12 +16,38 @@ public class UserDAO {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void readAndprintUsers(){
+    public void readAndprintUsers() {
         String sql = "Select * from user_detail";
         List<User> users = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(User.class));
-
         users.forEach(System.out::println);
+    }
 
+    public int addUser(User user) {
+
+        String sql = "insert into user_detail values(?,?,?)";
+        return jdbcTemplate.update(sql, user.getId(), user.getFirstName(), user.getLastName());
 
     }
+    public int updateUser(User user) {
+
+        String sql = "update user_detail set last_name = ? where id = ?";
+        return jdbcTemplate.update(sql, user.getLastName(), user.getId());
+    }
+    public User findUserById(int id) {
+        String sql = "Select * from user_detail where ID = ?";
+        return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>( User.class), new Object[]{id});
+    }
+
+    public void creatStudentTable(){
+        String sql = "create table student(id int PRIMARY KEY , first_name varchar(50), last_name varchar(50), course_name varchar(50), rnk int)";
+        jdbcTemplate.execute(sql);
+    }
+
+   public List<Integer> findAllByLastName(String lastname){
+
+       return jdbcTemplate.queryForList(
+                "select id  from user_detail where last_name = ?",
+                Integer.class, lastname);
+   }
+
 }
